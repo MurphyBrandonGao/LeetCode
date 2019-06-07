ccac82f701d04c45e28116ec39abd156d6b9a118
GaoBo <18279552824@163.com>
2019-06-07 13:22:17 +0800

    19 solved

---
+++ LinkedList/19-Remove-Nth-Node-From-End-of-List/src/Solution.java
@@ -0,0 +1,21 @@
+/**
+ * @author Dell
+ * @create 2019-06-07 11:01
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (n-- > 0) {
+            fast = fast.next;
+        }
+        if (fast == null)
+            return head.next;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
