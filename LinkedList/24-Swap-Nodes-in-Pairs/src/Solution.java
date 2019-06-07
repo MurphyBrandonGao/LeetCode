f5e5599c40ff7ab1adb6b6c72f69291e2ed5dd7e
GaoBo <18279552824@163.com>
2019-06-07 13:24:02 +0800

    24 solved

---
+++ LinkedList/24-Swap-Nodes-in-Pairs/src/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * @author Dell
+ * @create 2019-06-07 11:40
+ */
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode node = new ListNode(-1);
+        node.next = head;
+        ListNode pre = node;
+        while (pre.next != null && pre.next.next != null) {
+            ListNode l1 = pre.next;
+            ListNode l2 = pre.next.next;
+            ListNode next = l2.next;
+            l1.next = next;
+            l2.next = l1;
+            pre.next = l2;
+
+            pre = l1;
+        }
+        return node.next;
+    }
+}
