a065e52f54c2f9c5d9476043525a164d46991907
GaoBo <18279552824@163.com>
2019-06-07 13:25:14 +0800

    160 solved

---
+++ LinkedList/160-Intersection-of-Two-Linked-Lists/src/Solution.java
@@ -0,0 +1,15 @@
+/**
+ * @author Dell
+ * @create 2019-06-06 23:56
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode l1 = headA;
+        ListNode l2 = headB;
+        while (l1 != l2) {
+            l1 = (l1 == null) ? headB : l1.next;
+            l2 = (l2 == null) ? headA : l2.next;
+        }
+        return l1;
+    }
+}
