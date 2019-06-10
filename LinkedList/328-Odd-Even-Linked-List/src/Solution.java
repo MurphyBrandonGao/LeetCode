381f63d1597e28abbb6f438cb048e02bfc565184
GaoBo <18279552824@163.com>
2019-06-10 23:58:18 +0800

    328 solved

---
+++ LinkedList/328-Odd-Even-Linked-List/src/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * @author Dell
+ * @create 2019-06-08 13:44
+ */
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
