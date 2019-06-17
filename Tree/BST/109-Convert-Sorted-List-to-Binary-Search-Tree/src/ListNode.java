700611f3ee9b5dd5f9e1c2af4b0af28c0c08afe9
GaoBo <18279552824@163.com>
2019-06-17 23:50:58 +0800

    109 solved

---
+++ Tree/BST/109-Convert-Sorted-List-to-Binary-Search-Tree/src/ListNode.java
@@ -0,0 +1,11 @@
+/**
+ * @author Dell
+ * @create 2019-06-17 16:16
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ Tree/BST/109-Convert-Sorted-List-to-Binary-Search-Tree/src/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * @author Dell
+ * @create 2019-06-17 16:14
+ */
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null)
+            return null;
+        if (head.next == null)
+            return new TreeNode(head.val);
+        ListNode preMid = preMid(head);
+        ListNode mid = preMid.next;
+        preMid.next = null;
+        TreeNode t = new TreeNode(mid.val);
+        t.left = sortedListToBST(head);
+        t.right = sortedListToBST(mid.next);
+        return t;
+    }
+
+    private ListNode preMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        ListNode pre = head;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return pre;
+    }
+}
