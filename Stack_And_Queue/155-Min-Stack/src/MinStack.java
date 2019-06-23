c09d02850a918406f35f916e10cc984d3e5fc766
GaoBo <18279552824@163.com>
2019-06-23 20:22:26 +0800

    155 sovled

---
+++ Stack_And_Queue/155-Min-Stack/src/MinStack.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+/**
+ * @author Dell
+ * @create 2019-06-18 20:02
+ */
+public class MinStack {
+    private Stack<Integer> dataStack;
+    private Stack<Integer> minStack;
+    private int min;
+
+    public MinStack() {
+        dataStack = new Stack<>();
+        minStack = new Stack<>();
+        min = Integer.MAX_VALUE;
+    }
+
+    public void push(int x) {
+        dataStack.push(x);
+        min = Math.min(x, min);
+        minStack.push(min);
+    }
+
+    public void pop() {
+        dataStack.pop();
+        minStack.pop();
+        min = minStack.isEmpty() ? Integer.MAX_VALUE : minStack.peek();
+    }
+
+    public int top() {
+        return dataStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
