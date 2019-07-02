7f8890c048efafa5210de9c3dbfc46295ddf6f2e
GaoBo <18279552824@163.com>
2019-07-02 23:05:53 +0800

    566 solved

---
+++ Array_And_Matrix/566-Resharp-the-Matrix/src/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * @author Dell
+ * @create 2019-06-24 11:13
+ */
+public class Solution {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int m = nums.length;
+        int n = nums[0].length;
+        if (m * n != r * c) {
+            return nums;
+        }
+
+        int[][] reshapedNums = new int[r][c];
+        int index = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                reshapedNums[i][j] = nums[index / n][index % n];
+                index++;
+            }
+        }
+        return reshapedNums;
+    }
+}
