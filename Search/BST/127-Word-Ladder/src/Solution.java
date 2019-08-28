2852b976486533318a2d8a181e77cc69efcccbd2
GaoBo <18279552824@163.com>
2019-08-28 19:36:36 +0800

    127 solved

---
+++ Search/BST/127-Word-Ladder/src/Solution.java
@@ -0,0 +1,115 @@
+import java.util.*;
+
+/**
+ * @author Dell
+ * @create 2019-08-28 19:10
+ */
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> beginSet = new HashSet<>();
+        Set<String> endSet = new HashSet<>();
+
+        int len = 1;
+        int strLen = beginWord.length();
+        HashSet<String> visited = new HashSet<>();
+
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            if (beginSet.size() > endSet.size()) {
+                Set<String> set = beginSet;
+                beginSet = endSet;
+                endSet = set;
+            }
+
+            Set<String> temp = new HashSet<String>();
+            for (String word : beginSet) {
+                char[] chs = word.toCharArray();
+
+                for (int i = 0; i < chs.length; i++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        char old = chs[i];
+                        chs[i] = c;
+                        String target = String.valueOf(chs);
+
+                        if (endSet.contains(target)) {
+                            return len + 1;
+                        }
+
+                        if (!visited.contains(target) && wordList.contains(target)) {
+                            temp.add(target);
+                            visited.add(target);
+                        }
+                        chs[i] = old;
+                    }
+                }
+            }
+
+            beginSet = temp;
+            len++;
+        }
+
+        return 0;
+    }
+
+    public int ladderLength_1(String beginWord, String endWord, List<String> wordList) {
+        wordList.add(beginWord);
+        int N = wordList.size();
+        int start = N - 1;
+        int end = 0;
+        while (end < N && !wordList.get(end).equals(endWord))
+            end++;
+
+        if (end == N)
+            return 0;
+
+        List<Integer>[] graphic = buildGraphic(wordList);
+        return getShortestPath(graphic, start, end);
+    }
+
+    private int getShortestPath(List<Integer>[] graphic, int start, int end) {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] marked = new boolean[graphic.length];
+        queue.add(start);
+        marked[start] = true;
+        int path = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            path++;
+            while (size-- > 0) {
+                int cur = queue.poll();
+                for (int next : graphic[cur]) {
+                    if (next == end)
+                        return path;
+                    if (marked[next])
+                        continue;
+                    marked[next] = true;
+                    queue.add(next);
+                }
+            }
+        }
+        return 0;
+    }
+
+    private List<Integer>[] buildGraphic(List<String> wordList) {
+        int N = wordList.size();
+        List<Integer>[] graphic = new List[N];
+        for (int i = 0; i < N; i++) {
+            graphic[i] = new ArrayList<>();
+            for (int j = 0; j < N; j++) {
+                if (isConnect(wordList.get(i), wordList.get(j)))
+                    graphic[i].add(j);
+            }
+        }
+        return graphic;
+    }
+
+    private boolean isConnect(String s1, String s2) {
+        int diffCnt = 0;
+        for (int i = 0; i < s1.length() && diffCnt <= 1; i++) {
+            if (s1.charAt(i) != s2.charAt(i))
+                diffCnt++;
+        }
+        return diffCnt == 1;
+    }
+}
